26f27ab0b01cfec930fd659baa80a81232cc9d70
kunal812eee <kunallamage@gmail.com>
2024-12-19 11:46:58 +0530

    updated onclick issue

--- asset/theme/indusguru/js/main.js
+++ asset/theme/indusguru/js/main.js
@@ -85,7 +85,7 @@
 
         // close menu clicking outside the menu itself
         siteBody.on('click', function(e){
-            if( !$(e.target).is('.header-nav, .header-nav__content, .header-menu-toggle, .header-menu-toggle span') ) {
+            if( !$(e.target).is('.header-nav, .header-nav__content, .header-menu-toggle, .header-menu-toggle span, .header-nav__list li, .plus-sign') ) {
                 // menuTrigger.removeClass('is-clicked');
                 siteBody.removeClass('menu-is-open');
             }
